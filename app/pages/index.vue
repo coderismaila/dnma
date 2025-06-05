111d43bc0c41382e2fc2b8ae31a27019c5c11bdc
Ismaila Hassan <code.ismailah28@gmail.com>
2025-06-05 04:24:49 +0100

    setup: add shadcn-nuxt

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -7,5 +7,6 @@
     <h1>
       Welcome to <strong>Nuxt 3</strong> with <strong>TypeScript</strong>!
     </h1>
+    <Button>Click me!</Button>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  modules: ["@nuxthub/core", "@nuxt/eslint", "@nuxt/icon"],
+  modules: ["@nuxthub/core", "@nuxt/eslint", "@nuxt/icon", "shadcn-nuxt"],
 
   devtools: { enabled: true },
 
@@ -7,6 +9,19 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-03-01",
 
   hub: {},
+
+  css: ["./app/assets/css/main.css"],
+
+  shadcn: {
+    prefix: "",
+    componentDir: "./app/components/ui",
+  },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 
   eslint: {
     config: {
